53e815ef47e1f7e4604cf0e1f7fbb39cfc5baa23
Ciril Sreedhar <ciril.sreedhar@sreyas.com>
2019-07-05 14:43:44 +0530

    changes

--- resources/views/backend/layouts/app.blade.php
+++ resources/views/backend/layouts/app.blade.php
@@ -4,21 +4,43 @@
 <head>
     <meta charset="UTF-8">
     <title>Admin Panel - @yield('title')</title>
-    <link rel="stylesheet" type="text/css" href="{{ asset('css/admin/styles.css') }}" />
-    <link rel="stylesheet" type="text/css" href="{{ asset('css/styles.css') }}" />
+    <!--Import Google Icon Font-->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/backend/styles.css') }}" />
 </head>
 
 <body>
   
-  @section('header')
-    <header>
-      Header Content
-    </header>
-  @show
-
   @section('side_navigation')
     <div class="side-nav">
-      Side Navigation
+      <ul id="main-nav" class="sidenav sidenav-fixed">
+        <li class="bold"><a href="#" class="waves-effect waves-teal">Mobile</a></li>
+      </ul>
+    </div>
+  @show
+  @section('top_bar')
+    <div class="navbar-fixed">
+      <nav class="navbar top-menu">
+        <div class="nav-wrapper"><a href="#!" class="brand-logo grey-text text-darken-4">Logo</a>
+        <ul id="top-right">
+          <li><a href='#' >user</a></li>
+          <li><a href="#" class="dropdown-toggle icon-item" data-toggle="dropdown">
+            <i class="material-icons ">notifications</i>
+            <span class="badge top-right">4</span>
+          </a></li>
+          <li><a class='dropdown-trigger' href='#' data-target='user-drop'><i class="material-icons">settings</i></a>
+            <ul id='user-drop' class='dropdown-content'>
+              <li><a href="#!">one</a></li>
+              <li><a href="#!">two</a></li>
+              <li class="divider" tabindex="-1"></li>
+              <li><a href="#!">three</a></li>
+              <li><a href="#!"><i class="material-icons">view_module</i>four</a></li>
+              <li><a href="#!"><i class="material-icons">cloud</i>five</a></li>
+            </ul>
+          </li>
+        </ul>
+        <a href="#" data-target="nav-mobile" class="top-nav sidenav-trigger waves-effect waves-light circle hide-on-large-only"><i class="material-icons">menu</i></a> </div>
+      </nav>
     </div>
   @show
   <div class="content">
@@ -29,9 +51,10 @@
    Footer Content  
   </footer>
   @show
+  <script src="{{ asset('js/backend/scripts.js') }}"></script>
   @section('page_script')
   @show
 </body>
-<!-- <script src="{{ asset('js/admin/scripts.js') }}"></script> -->
+
 
 </html>
--- webpack.mix.js
+++ webpack.mix.js
@@ -12,4 +12,13 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+    .sass('resources/sass/app.scss', 'public/css')
+    .styles([
+	    'resources/css/backend/materialize.min.css',
+	    'resources/css/backend/custom.css'
+	], 'public/css/backend/styles.css')
+	.scripts([
+		'resources/js/jquery-3.4.1.min.js',
+	    'resources/js/backend/materialize.min.js',
+	    'resources/js/backend/custom.js'
+	], 'public/js/backend/scripts.js');
